8d1cc49fe272c3b622de23b0e27640c30b790f7f
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2025-12-01 07:24:07 +0700

    create book controller

--- be_koshunter/controller/book.controller.js
+++ be_koshunter/controller/book.controller.js
@@ -1,3 +1,50 @@
 const bookModel = require(`../models/index`).book
+const kosModel = require(`../models/index`).kos
+const userModel = require(`../models/index`).user
 const Op = require('sequelize').Op
 
+exports.getAllBook = async (request, response) => {
+    try {
+        const books = await bookModel.findAll({
+            include: [
+                { model: kosModel },
+                { model: userModel }
+            ],
+            order: [['createdAt', 'DESC']]
+        })
+
+        if (books.length === 0) {
+            return response.status(404).json({
+                status: false,
+                message: `No data to load`
+            })
+        }
+
+        return response.status(200).json({
+            status: true,
+            data: books,
+            message: `Bookings have been loaded`
+        })
+    } catch (error) {
+        return response.status(500).json({
+            status: false,
+            message: error.message
+        })
+    }
+}
+
+exports.createBook = async (request, response) => {
+
+}
+
+exports.findBook = async (request, response) => {
+
+}
+
+exports.updateBook = async (request, response) => {
+
+}
+
+exports.deleteBook = async (request, response) => {
+
+}
--- be_koshunter/routes/book.route.js
+++ be_koshunter/routes/book.route.js
@@ -3,3 +3,6 @@ const bookController = require(`../controller/book.controller`)
 const { authenticateToken, authorizeRoles } = require('../middleware/auth.middleware')
 const app = express()
 
+app.get("/", authenticateToken, bookController.getAllBook)
+
+module.exports = app
