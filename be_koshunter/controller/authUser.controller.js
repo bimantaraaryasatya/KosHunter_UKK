adb6a56203773adda602d95f3dd457aa01275b66
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2026-01-11 19:00:18 +0700

    update flow register

--- be_koshunter/controller/authUser.controller.js
+++ be_koshunter/controller/authUser.controller.js
@@ -5,15 +5,40 @@ const jwt = require(`jsonwebtoken`)
 exports.register = async (request, response) => {
     try {
         const { name, email, password, phone } = request.body
+        const existingUser = await userModel.findOne({ where: { email } })
+        if (request.role === "owner") {
+            if (!existingUser) {
+                return response.status(403).json({
+                    status: false,
+                    message: "You must register as society first"
+                })
+            }
+
+            if (existingUser.role !== "society") {
+                return response.status(403).json({
+                    status: false,
+                    message: "Only society can upgrade to owner"
+                })
+            }
+        }
         const hashedPassword = await bcrypt.hash(password, 10)
-        const existingEmail = await userModel.findOne({where: {email}})
-        if (existingEmail) {
-            return response.status(400).json({message: 'Email already exists'})
+        if (existingUser && request.role === "society") {
+            return response.status(400).json({
+                status: false,
+                message: 'Email already exists'
+            })
         }
         const user = await userModel.create({name, email, password: hashedPassword, phone, role: request.role})
-        response.status(201).json({status: true, message: 'Guest registered!', data: user})
+        response.status(201).json({
+            status: true, 
+            data: user,
+            message: 'Guest registered!'
+        })
     } catch (error) {
-        response.status(500).json({error: error.message})
+        response.status(500).json({
+            status: false,
+            error: error.message
+        })
     }
 }
 
@@ -22,12 +47,18 @@ exports.login = async (request, response) => {
         const { email, password } = request.body
         const user = await userModel.findOne({where: {email}})
         if (!user) {
-            return response.status(401).json({error: "Invalid credentials"})
+            return response.status(401).json({
+                status: false,
+                error: "Invalid credentials"
+            })
         }
 
         const validPassword = await bcrypt.compare(password, user.password)
         if (!validPassword) {
-            return response.status(401).json({error: "Invalid credentials"})
+            return response.status(401).json({
+                status: false,
+                error: "Invalid credentials"
+            })
         }
 
         const token = jwt.sign({id: user.id, name: user.name, email: user.email, role: user.role}, process.env.JWT_SECRET, { expiresIn: "24h" })
@@ -38,6 +69,9 @@ exports.login = async (request, response) => {
             token: token
         })
     } catch (error) {
-        response.status(500).json({error: error.message})
+        response.status(500).json({
+            status: false,
+            error: error.message
+        })
     }
 }
