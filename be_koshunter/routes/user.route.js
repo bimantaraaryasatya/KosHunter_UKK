4239bfa47726c0420d9c3b9d6d3ddc55bb76a191
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2026-01-15 12:42:04 +0700

    add multer in user route

--- be_koshunter/routes/user.route.js
+++ be_koshunter/routes/user.route.js
@@ -8,7 +8,7 @@ const app = express()
 app.get("/", authenticateToken, userController.getAllUser)
 app.post("/find", authenticateToken, userController.findUser)
 app.post("/", authenticateToken, authorizeRoles('admin'), upload.none(), userController.createUser)
-app.put("/:id", authenticateToken, userController.updateUser)
+app.put("/:id", authenticateToken, authorizeRoles('owner', 'admin'), upload.none(), userController.updateUser)
 app.delete("/:id", authenticateToken, authorizeRoles('society', 'admin'), userController.deleteUser)
 
 module.exports = app
